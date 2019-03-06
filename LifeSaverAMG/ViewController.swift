77e5ecb4db791c914de16f55e3ddcb21f7bce7f8
Adam Garcia <amg16h@lib-301-24-9308.local>
2019-03-05 21:30:05 -0600

    Initial Commit

---
+++ LifeSaverAMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeSaverAMG
+//
+//  Created by Adam Garcia on 3/5/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
