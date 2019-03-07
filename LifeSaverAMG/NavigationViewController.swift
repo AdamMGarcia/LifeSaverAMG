0d1e2b0711ad471612702377a250be1ef6a99ec5
Adam Garcia <amg16h@lib-301-21-9305.local>
2019-03-06 23:59:02 -0600

    Created code for color background and created UINavigationController

---
+++ LifeSaverAMG/NavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationViewController.swift
+//  LifeSaverAMG
+//
+//  Created by Adam Garcia on 3/6/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LifeSaverAMG/ViewController.swift
+++ LifeSaverAMG/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //changes background color of the app
+        self.view.backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
     }
 
 
